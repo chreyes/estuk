50a53f73d2f28a48885647d6a995b2c60b322c18
chreyes <chreyes@gmail.com>
2015-08-13 15:52:57 -0300

    added sales model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,11 @@ class Book < ActiveRecord::Base
 
 	friendly_id :name, use: :slugged
 	belongs_to :user
+	has_many :sales
 
 	has_attached_file :image
 	has_attached_file :resource
+
+	validates_numericallity_of :price,
+	greater_than: 49, message: 'must be greatear than 50 cents' 	
 end
