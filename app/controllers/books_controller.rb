d2fab277927c73e9e8bc1c29656849de081c4f1b
chreyes <chreyes@gmail.com>
2015-08-13 11:01:11 -0300

    Dashboard added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  respond_to :html
 
   # GET /books
   # GET /books.json
@@ -27,7 +29,7 @@ class BooksController < ApplicationController
     @book = current_user.books.new(book_params)
 
     @book.save
-    	respond_with(@book)
+    respond_with(@book)
   end
 
   # PATCH/PUT /books/1
